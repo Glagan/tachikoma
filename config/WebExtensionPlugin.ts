2507b0ea06fc72c4d0d0ce012a020fbb58e2e853
Glagan <Glagan@users.noreply.github.com>
2022-06-16 18:10:22 +0200

    Fix duplicate scripts in WebExtensionPlugin on watch mode

--- config/WebExtensionPlugin.ts
+++ config/WebExtensionPlugin.ts
@@ -65,14 +65,48 @@ export default class WebExtensionPlugin {
 		}
 
 		compiler.hooks.thisCompilation.tap(pluginName, (thisCompilation) => {
+			// Reset update manifest values to avoid duplicates in watch mode
+			thisCompilation.hooks.optimizeChunkAssets.tap(pluginName, (chunks) => {
+				for (const entry of this.resources.entries) {
+					let [reference, key] = this.getReference(entry.path);
+					if (entry.mode == "script") {
+						reference[key] = "";
+					} else if (entry.mode == "script_list") {
+						reference[key] = [];
+					} else {
+						if (reference[key].js) {
+							delete reference[key].js;
+						}
+						if (reference[key].css) {
+							delete reference[key].css;
+						}
+					}
+				}
+			});
+
+			// Process each final chunks and get the list of files associated to each entry points
 			thisCompilation.hooks.afterOptimizeChunkAssets.tap(pluginName, (chunks) => {
-				// Process each final chunks and get the list of files associated to each entry points
 				const rChunks = Array.from(chunks).reverse();
 				for (const chunk of rChunks) {
 					const scripts = Array.from(chunk.files).filter((file) => file.endsWith(".js"));
-					let entry = this.resources.entries.find((entry) => entry.name == chunk.name);
-					if (entry) {
-						// ? A reference without a key is a content_script
+					const styles = Array.from(chunk.files)
+						.filter((file) => file.endsWith(".css"))
+						.concat(Array.from(chunk.auxiliaryFiles).filter((file) => file.endsWith(".css")));
+					// Collect runtimes or the entrypoint itself
+					let runtimes: string[] = [];
+					if (chunk.name) {
+						runtimes.push(chunk.name);
+					} else {
+						if (!chunk.runtime) continue;
+						if (typeof chunk.runtime === "string") {
+							runtimes.push(chunk.runtime);
+						} else {
+							runtimes.push(...Array.from(chunk.runtime));
+						}
+					}
+					// Check and assign each scripts and css files for each entrypoints
+					for (const runtime of Array.from(runtimes!)) {
+						let entry = this.resources.entries.find((entry) => entry.name == runtime);
 						if (!entry) continue;
 						let [reference, key] = this.getReference(entry.path);
 						if (entry.mode == "script") {
@@ -84,9 +118,6 @@ export default class WebExtensionPlugin {
 								reference[key].js = [];
 							}
 							reference[key].js.push(...scripts);
-							const styles = Array.from(chunk.files)
-								.filter((file) => file.endsWith(".css"))
-								.concat(Array.from(chunk.auxiliaryFiles).filter((file) => file.endsWith(".css")));
 							if (styles.length > 0) {
 								if (!reference[key].css) {
 									reference[key].css = [];
@@ -94,38 +125,11 @@ export default class WebExtensionPlugin {
 								reference[key].css.push(...styles);
 							}
 						}
-					} else {
-						// Add chunk without name to all runtimes that include it
-						let runtimes = chunk.runtime;
-						if (!runtimes) continue;
-						if (typeof runtimes === "string") {
-							runtimes = [runtimes] as any;
-						}
-						for (const runtime of Array.from(runtimes!)) {
-							let entry = this.resources.entries.find((entry) => entry.name == runtime);
-							if (!entry) continue;
-							let [reference, key] = this.getReference(entry.path);
-							if (entry.mode == "script") {
-								reference[key] = scripts[0];
-							} else if (entry.mode == "script_list") {
-								reference[key].push(...scripts);
-							} else {
-								if (!reference[key].js) {
-									reference[key].js = [];
-								}
-								reference[key].js.push(...scripts);
-								const styles = Array.from(chunk.files)
-									.filter((file) => file.endsWith(".css"))
-									.concat(Array.from(chunk.auxiliaryFiles).filter((file) => file.endsWith(".css")));
-								if (styles.length > 0) {
-									if (!reference[key].css) {
-										reference[key].css = [];
-									}
-									reference[key].css.push(...styles);
-								}
-							}
-						}
 					}
+
+					// TODO options.exposeIcons
+					// TODO Expose auxiliaryFiles that are *not* scripts and styles and add them to web_accessible_resources
+					// TODO -- along with emitting them as assets ?
 
 					// TODO auxiliaryFiles as assets
 					// * chunk.auxiliaryFiles
